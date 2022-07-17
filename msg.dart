0b28ef57c94aa3ccf8475c65db47d0ec3121775a
Priyanka Karna <65607915+priyankakarna@users.noreply.github.com>
2022-07-17 22:25:53 +0530

    Add files via upload

---
+++ msg.dart
@@ -0,0 +1,19 @@
+// import 'package:flutter/material.dart';
+
+// import './message.dart';
+
+// void main() => runApp(MsgApp());
+
+// class MsgApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(body: Design()());
+//   }
+// }
+
+// class Design extends StatelessWidget{
+//   @override 
+//   Widget build(BuildContext context){
+//     return 
+//   }
+// }
